7dfabccb0a4f903b89c46377e9a49b42162d8691
Syntarex <4340303+Syntarex@users.noreply.github.com>
2023-11-10 12:53:19 +0100

    Fix Bug

--- src/util/wait-for-element.util.ts
+++ src/util/wait-for-element.util.ts
@@ -2,17 +2,17 @@
  * Wait for an element existing in the DOM.
  * Example: const htmlElement = await waitForElement("#footer .link");
  * */
-export const waitForElement = async (selector: string): Promise<Element | null> => {
+export const waitForElement = async (selector: string): Promise<HTMLElement | null> => {
     return new Promise((resolve) => {
         if (document.querySelector(selector)) {
-            resolve(document.querySelector(selector));
+            resolve(document.querySelector(selector) as HTMLElement | null);
             return;
         }
 
         const observer = new MutationObserver((mutations) => {
             if (document.querySelector(selector)) {
                 observer.disconnect();
-                resolve(document.querySelector(selector));
+                resolve(document.querySelector(selector) as HTMLElement | null);
             }
         });
 

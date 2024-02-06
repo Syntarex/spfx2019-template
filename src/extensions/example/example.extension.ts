60182220d3b382d3ae48e2edf81f57705bdbc553
Syntarex <4340303+Syntarex@users.noreply.github.com>
2024-02-06 11:48:52 +0100

    Füge insertCss-Beispiel hinzu

--- src/extensions/example/example.extension.ts
+++ src/extensions/example/example.extension.ts
@@ -1,4 +1,10 @@
 import { BaseApplicationCustomizer } from "@microsoft/sp-application-base";
+import { insertCss } from "insert-css";
+
+// Muss nicht in onInit leben, da keine SPFx-Funktionalität genutzt wird
+insertCss(`
+    #spSiteHeader { display: none; }
+`);
 
 export default class ExampleApplicationCustomizer extends BaseApplicationCustomizer<unknown> {
     public async onInit(): Promise<void> {
